bf20a0c35a05c53cbef8c39d6384e97e1481162c
ColeSteez <collinsappiahanokye@gmail.com>
2023-08-21 11:42:45 -0700

    Printf Files

---
+++ get_size.c
@@ -0,0 +1,64 @@
+#include "main.h"
+
+/**
+ * get_size - Parse format string to get size specifier
+ * @format: Format string
+ * @i: Index of current character in format string
+ *
+ * Return: Calculated size specifier
+ */
+int get_size(const char *format, int *i)
+{
+	int curr_i = *i + 1;
+	int size = 0;
+
+	if (format[curr_i] == 'l')
+		size = S_LONG;
+	else if (format[curr_i] == 'h')
+		size = S_SHORT;
+
+	if (size == 0)
+		*i = curr_i - 1;
+	else
+		*i = curr_i;
+
+	return (size);
+}
+
+get_width.c
+#include "main.h"
+
+/**
+ * get_width - Parse format string to get width
+ * @format: Format string
+ * @i: Index of current character in format string
+ * @list: list of arguments
+ *
+ * Return: Calculated width
+ */
+int get_width(const char *format, int *i, va_list list)
+{
+	int curr_i;
+	int width = 0;
+
+	for (curr_i = *i + 1; format[curr_i] != '\0'; curr_i++)
+	{
+		if (is_digit(format[curr_i]))
+		{
+			width *= 10;
+			width += format[curr_i] - '0';
+		}
+		else if (format[curr_i] == '*')
+		{
+			curr_i++;
+			width = va_arg(list, int);
+			break;
+		}
+		else
+			break;
+	}
+
+	*i = curr_i - 1;
+
+	return (width);
+}
